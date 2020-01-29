17c943c0d525e0caeccc480a7c745ec670b350b0
Your Name <you@example.com>
2020-01-29 15:12:32 +0200

    repo

---
+++ js/app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+$(document).ready(function () {
+
+    new WOW().init();
+
+    
+    $('.ba-slider-wrapper').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        adaptiveHeight: true,
+        arrows: false
+    });
+    
+
+});
+   
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
